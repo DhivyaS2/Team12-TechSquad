9fc531fa20f5f4000f7be4d7768a4ed7f0affee4
preetha012 <preetha012@gmail.com>
2024-07-13 12:35:23 -0400

    added LCHF

---
+++ Team12TechSquad/src/test/java/LCHF/LCHF_elim_add_test.java
@@ -0,0 +1,145 @@
+package LCHF;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import LCHF.commonBaseAtoZ;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Base.Browser;
+import pageobjects.recipes;
+
+public class LCHF_elim_add_test {
+	
+	public WebDriver driver;
+	public  ArrayList<String> elimateLCHF = new ArrayList<>();
+	public  List<String> AddListLCHF  = new ArrayList<>();
+	public commonBaseAtoZ commonAtoZObj = new commonBaseAtoZ();
+	public  int Keto_receipe_count=0;
+	
+	public static boolean containsEliminatedIngredient(List<String> recipeIngredients, List<String> eliminationList) {
+	    for (String ingredient : eliminationList) {
+	        if (recipeIngredients.contains(ingredient)) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+	public static boolean containsAddIngredient(List<String> recipeIngredients, List<String> AddList) {
+	    for (String ingredient : AddList) {
+	        if (recipeIngredients.contains(ingredient)) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+
+	//Iterate througough all the urls Collected......
+	public  void EliminateLCHF_fn(ArrayList<String> AllreceipeUrlList) throws InterruptedException, IOException {
+		
+		AddListLCHF.add("fish");
+		AddListLCHF.add("paneer");
+		AddListLCHF.add("butter");
+		AddListLCHF.add("onion");
+		
+		
+		elimateLCHF = readLCHFEliminateData();
+		System.out.println("eliminate array: "+elimateLCHF);
+		List<String> ingrediantsList = new ArrayList<> ();
+		
+		for(String eachUrl : AllreceipeUrlList) {
+			ingrediantsList.clear();
+			System.out.println("Receipe Url : "+eachUrl);
+			Thread.sleep(2000);
+			driver.navigate().to(eachUrl);
+			List<WebElement> recp_ingredns = driver.findElements(By.xpath("//div[@id='rcpinglist']//span"));
+			for (WebElement each_ingrediant : recp_ingredns) {
+				String ingrediant = each_ingrediant.getText().toLowerCase();
+				ingrediantsList.add(ingrediant);
+			}
+			
+			//System.out.println("Ingrediants : "+ingrediantsList);
+				
+			
+			boolean containsEliminatedIngredient = containsEliminatedIngredient(ingrediantsList, elimateLCHF);
+			
+			// Print the result
+			try {
+				if(!containsEliminatedIngredient) {
+					boolean containsAddIngredient = containsAddIngredient(ingrediantsList, AddListLCHF);
+					
+					System.out.println("****** "+AddListLCHF);
+					if(containsAddIngredient) {
+						 Keto_receipe_count= Keto_receipe_count+1;
+						//System.out.println("The recipe does not contain any ingredients from the elimination list.");
+			           
+						String receipe_Name = driver.findElement(By.xpath("//span[@id='ctl00_cntrightpanel_lblRecipeName']")).getText();
+						String prep_time = driver.findElement(By.cssSelector("time[itemprop='prepTime']")).getText();
+						String cook_time = driver.findElement(By.cssSelector("time[itemprop='cookTime']")).getText();
+						String tags = driver.findElement(By.xpath("//div[@id='recipe_tags']/a/span")).getText();
+						System.out.println("**Receipe Name:  " + receipe_Name);
+						System.out.println("**prep_time:  " + prep_time);
+						System.out.println("**cook_time:  " + cook_time);
+						System.out.println("**tags:  " + tags);
+						
+			            
+					}else {
+						System.out.println("Ingrediants not in add List");
+					}
+	        	       		      	
+	            
+				} else {
+	        	System.out.println("The recipe contain any ingredients from the elimination list.");
+	            
+				}
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+			
+			System.out.println("No: of Keto receipe scrapped: "+Keto_receipe_count);
+			}
+			//}
+			
+			
+			
+		
+		}
+	
+	public  ArrayList readLCHFEliminateData() throws IOException {
+		
+		String excelPath = "C:\\Users\\anoop\\Downloads\\IngrediantsEliminate.xlsx";
+	    FileInputStream inputStream = new FileInputStream(excelPath);
+	    XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+	    XSSFSheet sheet = workbook.getSheet("LCHFElimination ");  
+	    int rows = sheet.getLastRowNum();
+	    for (int r = 2; r <= rows; r++) {
+	        Row row = sheet.getRow(r);
+	        Cell cell = row.getCell(0);
+	        if (cell != null && cell.getCellType() != CellType.BLANK) {
+	            String value = cell.getStringCellValue();
+	            String[] splitArray = value.split(",");
+	            for (String substring: splitArray) {
+	            	elimateLCHF.add(substring.trim().toLowerCase());
+	            	
+	            }
+	
+	        }
+	    }
+	    return elimateLCHF;
+	}
+	
+		 
+	}
+
---
+++ Team12TechSquad/src/test/java/LCHF/commonBaseAtoZ.java
@@ -0,0 +1,89 @@
+package LCHF;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Base.Browser;
+import pageobjects.recipes;
+import LCHF.LCHF_elim_add_test;
+
+
+public class commonBaseAtoZ {
+	
+	recipes r;
+	WebDriver driver;
+	public static final ArrayList<String> AllreceipeUrlList = new ArrayList<>();
+	public LCHF_elim_add_test LCHFObj = new LCHF_elim_add_test();
+	
+	 @BeforeTest
+	  public void launch() throws IOException {
+	  Browser b= new  Browser();
+	    b.beforescraping();
+	    b.launchsite();
+	   r= new recipes();
+	  }
+	 
+	 @Test(priority=0)
+	 public  ArrayList ScrappingAtoZ() {
+		 
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		Actions actions = new Actions(driver);
+		r.click_AtoZ(); 
+	//list of recipes in page
+	List<WebElement> list=	r.collect_recipelist();
+	List<String> Title = list.stream().map(s->s.getText()).collect(Collectors.toList());
+	//System.out.println(Title);	
+	
+	AllreceipeUrlList.clear();
+	driver.findElement(By.xpath("//a[normalize-space()='C']")).click();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+    WebElement currentPage = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class='rescurrpg']")));
+
+    boolean hasNextPage = true;
+    while (hasNextPage) {
+        try {
+            List<WebElement> CurrPageRecipeList = driver.findElements(By.xpath("//*[@class ='rcc_recipecard']"));
+            System.out.println("No: of receipes in currentPage:" + CurrPageRecipeList.size());
+
+            for (WebElement eachRecipe : CurrPageRecipeList) {
+                WebElement recipeNameElement = eachRecipe.findElement(By.xpath(".//span[@class='rcc_recipename']"));
+                List <WebElement> urlLinks = driver.findElements(By.xpath("//span[@class='rcc_recipename']/a"));
+                for (WebElement webElementlink : urlLinks) {
+					AllreceipeUrlList.add(webElementlink.getAttribute("href"));					
+                }	
+            }
+            try {
+                WebElement nextBtn = driver.findElement(By.xpath("//*[@class='rescurrpg']/following-sibling::a"));
+                nextBtn.click();
+                wait.until(ExpectedConditions.stalenessOf(currentPage));
+                currentPage = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class='rescurrpg']")));
+            } catch (Exception e) {
+                hasNextPage = false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            hasNextPage = false;
+        }
+    }
+    System.out.println("Total URLs in array -> "+AllreceipeUrlList.size());	
+	return AllreceipeUrlList;
+	 }
+	
+	public void callLCHFTestcase() throws InterruptedException, IOException {
+		LCHFObj.EliminateLCHF_fn(AllreceipeUrlList);
+		
+	}
+
+}
